042f851734b2d600bb58d805386a88e17b079f37
sangeethaSSS <sangeetha@shivasoftwares.com>
2023-06-15 18:14:09 +0530

    remove unwanted

--- projects/my-angular-library/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of my-angular-library
- */
-
-export * from './lib/my-angular-library.service';
-export * from './lib/my-angular-library.component';
-export * from './lib/my-angular-library.module';
